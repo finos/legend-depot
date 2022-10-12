1ad689e59356c734396a4e00499e95ede8a2452d
elopezcastro <82950460+elopezcastro@users.noreply.github.com>
2022-10-12 18:36:57 +0200

    Update VersionedEntitiesHandlerImpl.java

--- legend-depot-artifacts-refresh/src/main/java/org/finos/legend/depot/store/artifacts/services/entities/VersionedEntitiesHandlerImpl.java
+++ legend-depot-artifacts-refresh/src/main/java/org/finos/legend/depot/store/artifacts/services/entities/VersionedEntitiesHandlerImpl.java
@@ -19,6 +19,7 @@ import org.finos.legend.depot.domain.entity.EntityDefinition;
 import org.finos.legend.depot.domain.entity.StoredEntity;
 import org.finos.legend.depot.domain.project.ProjectData;
 import org.finos.legend.depot.services.api.entities.ManageEntitiesService;
+import org.finos.legend.depot.store.artifacts.api.entities.EntityArtifactsProvider;
 import org.finos.legend.depot.store.artifacts.api.entities.VersionedEntitiesArtifactsHandler;
 import org.finos.legend.depot.store.artifacts.api.entities.VersionedEntityArtifactsProvider;
 import org.finos.legend.sdlc.domain.model.entity.Entity;
