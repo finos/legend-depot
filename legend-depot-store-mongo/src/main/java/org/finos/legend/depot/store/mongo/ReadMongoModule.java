be6717a5ea499d5bc06f6bb6f5f98da7b56be433
Abhishoya Lunavat <Abhishoya.Lunavat@gs.com>
2022-12-15 13:10:48 +0530

    fix checkstyle

--- legend-depot-store-mongo/src/main/java/org/finos/legend/depot/store/mongo/ReadMongoModule.java
+++ legend-depot-store-mongo/src/main/java/org/finos/legend/depot/store/mongo/ReadMongoModule.java
@@ -1,3 +1,18 @@
+//  Copyright 2021 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 package org.finos.legend.depot.store.mongo;
 
 import com.google.inject.PrivateModule;
@@ -8,9 +23,11 @@ import org.finos.legend.depot.store.mongo.entities.EntitiesMongo;
 import org.finos.legend.depot.store.mongo.generation.file.FileGenerationsMongo;
 import org.finos.legend.depot.store.mongo.projects.ProjectsMongo;
 
-public class ReadMongoModule extends PrivateModule {
+public class ReadMongoModule extends PrivateModule
+{
     @Override
-    protected void configure() {
+    protected void configure()
+    {
         bind(Projects.class).to(ProjectsMongo.class);
         bind(Entities.class).to(EntitiesMongo.class);
         bind(FileGenerations.class).to(FileGenerationsMongo.class);
